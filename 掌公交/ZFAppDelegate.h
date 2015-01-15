c47458f6ca83c99c0f834e33d8a964c117ede6ff
zhangfei312 <zhangfeiccut@163.com>
2015-01-15 14:33:55 +0800

    Initial Commit

---
+++ 掌公交/ZFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZFAppDelegate.h
+//  掌公交
+//
+//  Created by froda on 15/1/15.
+//  Copyright (c) 2015年 froda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 掌公交/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  掌公交
+//
+//  Created by froda on 15/1/15.
+//  Copyright (c) 2015年 froda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZFAppDelegate class]));
+    }
+}
